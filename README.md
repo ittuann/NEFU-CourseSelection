99cdd1368670057da0a79e5fe5ae4c834fae6443
ITTUANN\ittuann <airwish@outlook.com>
2023-07-02 10:57:50 +0800

    Initial Version

---
+++ README.md
@@ -0,0 +1,73 @@
+# NEFU Fuxk Courses Selection
+
+这是一个使用 selenium 编写的一个简单的林带选课脚本，实现了多线程模拟点击自动登录，以及批量数据爬取的功能。
+
+# 使用方法
+
+以 Windows 为例：
+
+## 环境配置
+
+首先需要简单的环境配置：
+
+1. [下载脚本:](https://github.com/ittuann/NEFU-CoursesSelection/releases/latest) <https://github.com/ittuann/NEFU-CoursesSelection/releases/latest>
+
+2. 需要 Python 环境，然后安装`Selenium`
+
+   ```python
+   pip install selenium
+   ```
+
+3. 然后需要下载 `Chrome Driver`
+
+   打开 <https://chromedriver.chromium.org/downloads> 网页，找到适合的版本然后下载`chromedriver_win32.zip`
+
+4. 将压缩包解压，并复制`chromedriver.exe`的路径
+
+5. 修改脚本第16行`driver_path`变量，将路径更换成刚刚复制的内容。
+
+   例如，把默认的内容
+
+   ```python
+   driver_path = r"C:\Download\chromedriver_win32\chromedriver.exe"
+   ```
+
+   修改为
+
+   ```python
+   driver_path = r"D:\chromedriver.exe"
+   ```
+
+## 运行脚本
+
+首先在Windows中搜索并打开 “命令提示符”
+
+然后进入到脚本所在文件夹（文件夹的路径需要替换成你自己的）
+
+```shell
+cd C:\Users\Download\NEFUCourseSelection
+```
+
+之后即可运行脚本。
+
+```shell
+python main.py
+```
+
+脚本需要依次输入学号、密码、选课页面的网站、课程名称、是否使用WebVPN
+
+1. 选课页面的网站需要在教务系统中手动获得。在 `培养管理 -> 选课中心` ，复制`[进入选课]`的链接并粘贴到脚本即可。
+2. 课程名称可以输入单个或多个，每行一个名称。
+3. 是否使用WebVPN，取决于是否在校园网内。如果没连接校园网则必须使用，在校园网内则都可以。
+
+脚本的原理简单来说就是每0.5s左右轮询来选一次这门课，看有没有同学退课，或是有没有奸商在尝试卖课。如果长时间没有反应被弹出则自动重新登录。我测试的是在暑期小学期来抢通识教育选修课，挂一天抢到的概率还是蛮大的。
+
+脚本对于每个课程都创建了一个独立的线程来抢课。当然也可以同时运行多个。
+
+最后，使用脚本后果自负，以及Fuxk抢课来卖的同学
+
+# 运行截图
+
+![RunScreenshot1](.\img\RunScreenshot1.png)
+
+![RunScreenshot1](.\img\RunScreenshot2.png)
---
+++ main.py
@@ -0,0 +1,151 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import TimeoutException
+from time import sleep
+from datetime import datetime
+import threading
+
+driver_path = r"C:\Download\chromedriver_win32\chromedriver.exe"
+time_out = 60
+is_webvpn = True
+
+student_id = input("请输入学号")
+student_pwd = input("请输入密码")
+website = input("请输入选课页面的网址")
+select_names = []
+
+
+# 浏览器驱动配置，这里使用的是Chrome，使用时要注意浏览器驱动是否下载
+chrome_options = Options()
+chrome_options.add_argument('--headless')   # 无头模式
+
+
+def login():
+    try:
+        driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+
+        # 登录WebVPN
+        if is_webvpn:
+            driver.get('https://jwcnew.webvpn.nefu.edu.cn/dblydx_jsxsd/xk/AccessToXk')
+            driver.find_element_by_xpath('//*[@id="un"]').send_keys(str(student_id))
+            driver.find_element_by_xpath('//*[@id="pd"]').send_keys(str(student_pwd))
+            driver.find_element_by_xpath('//*[@id="rememberName"]').click()
+            driver.find_element_by_xpath('//*[@id="index_login_btn"]/input').click()
+            sleep(2)  # 登录成功延时2s
+
+            # 继续登录教务系统
+            driver.find_element_by_xpath('//*[@id="Form1"]/div/div/div[2]/div[1]/div[2]/input[1]').send_keys(
+                str(student_id))
+            driver.find_element_by_xpath('//*[@id="pwd"]').send_keys(str(student_pwd))
+            driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
+
+        else:
+            driver.get('https://jwcnew.nefu.edu.cn/dblydx_jsxsd/')  # 仅在本学校有效
+            driver.find_element_by_xpath('//*[@id="Form1"]/div/div/div[2]/div[1]/div[2]/input[1]').send_keys(
+                str(student_id))
+            driver.find_element_by_xpath('//*[@id="pwd"]').send_keys(str(student_pwd))
+            driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
+
+        print("登录成功！ %s" % datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+        sleep(2)  # 登录成功延时2s
+    except Exception as err:
+        print(str(err))
+        print("请检查账号密码")
+        return
+    return driver
+
+
+def select_course(web_page, select_name):
+    # 获取全部课程名称
+    driver = login()
+    driver.get(web_page)
+    element = driver.find_element_by_css_selector('#divFrmLeft')
+    tr = element.find_elements_by_xpath("//tr[contains(@id,'xk')]")
+    id_list = []
+    course_list = []
+    name_list = []
+    for td in tr:
+        id_list.append(td.get_attribute("id"))
+        course_list.append(td.text)
+    for course_name in course_list:
+        name_list.append(course_name.split()[4])  # 获得所有课程名字
+    name_id_dict = dict(zip(name_list, id_list))  # 课程与课程代码键值对
+    print("获取全部课程成功 %s" % datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+    if select_name in name_id_dict:
+        print("查询成功！正在选 %s 这门课 %s" % (select_name, datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]))
+    else:
+        print("查询失败,请确认该页面是否有该课程")
+        return
+
+    # 开始抢课
+    try:
+        attempt = 1
+        while True:
+            driver.find_element_by_xpath('//*[@id="%s"]/td[1]/a' % name_id_dict[select_name]).click()
+            alert = driver.switch_to.alert
+            alert.accept()
+
+            wait = WebDriverWait(driver, timeout=time_out)      # 显式等待下一个alert弹出的时间 频率为0.5s
+            alert = None
+            while alert is None:
+                try:
+                    alert = wait.until(ec.alert_is_present())   # 等待弹窗出现
+                except TimeoutException:
+                    driver.quit()
+                    print("重新登录中 %s" % datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+                    driver = login()
+                    print("重新登录成功！ %s" % datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+            if alert.text == '选课成功！':
+                print(alert.text)
+                break
+            msg = "第{}次尝试：{} {}".format(
+                attempt,
+                alert.text,
+                datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+            )
+            print(msg)
+            alert.accept()
+            attempt += 1
+    except Exception as e:
+        print(str(e))
+    driver.close()
+
+
+def run_threads(web_page, select__names):
+    threads = []
+    for select_name in select__names:
+        thread = threading.Thread(target=select_course, args=(web_page, select_name))
+        # 启动线程
+        thread.start()
+        threads.append(thread)
+    # 等待所有线程完成
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == '__main__':
+    # select_course(website, select_name)
+
+    # 处理输入
+    while True:
+        name = input("请输入课程名称 （名称打完后按回车完成输入，直接输入回车结束输入）：")
+        if name == "":
+            break
+        select_names.append(name)
+    webvpn_input = input("请输入是否使用校园外网WebVPN（True/False）：").lower()
+    if webvpn_input == "true":
+        is_webvpn = True
+    else:
+        is_webvpn = False
+
+    run_threads(website, select_names)
